74a0656518a3b483ac01282886bcde4b94bbe98d
dhanrajchopade <git@stackblitz.com>
2025-02-09 02:17:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Carbon-FP2-Append-list-by-reduce-method
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/dhanrajchopade/Carbon-FP2-Append-list-by-reduce-method)
---
+++ script.js
@@ -0,0 +1,26 @@
+const items = [
+  { id: 1, item: 'Item 1', price: 10 },
+  { id: 1, item: 'Item 2', price: 20 },
+  { id: 1, item: 'Item 3', price: 15 },
+  { id: 1, item: 'Item 4', price: 25 },
+];
+
+const itemListContainer = document.querySelector('#itemList');
+const totalCost = document.querySelector('#totalCost');
+const averageCost = document.querySelector('#averageCost');
+
+const mainitemList = items.reduce((acc,curr)=>{
+acc.totalCost += curr.price
+
+const itemList = document.createElement("li")
+itemList.innerHTML = `${curr.item} - $${curr.price}`
+itemListContainer.appendChild(itemList)
+
+const totalCost3 = acc
+return acc
+},{totalCost:0})
+
+const avgCost = mainitemList.totalCost/items.length
+
+ totalCost.innerHTML =mainitemList.totalCost
+averageCost.innerHTML = avgCost
